8166d0788f1e7876d979bdf3317ba67949c89c68
Michael Nnaji <mnnaji3@uwo.ca>
2021-02-12 20:43:19 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,4 +41,9 @@ class ApplicationController < Sinatra::Base
       redirect to "/articles/#{@article.id}"
     end
 
+    delete '/articles/:id' do 
+      Article.destroy(params[:id])
+      erb :show
+    end 
+
 end
